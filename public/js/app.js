f98adf7e21099b6c582080eb32952e4bd1a33294
RU$HABH <rnpatel2226@gmail.com>
2020-06-23 17:28:01 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     errorMsg.textContent = ""
     
     // fetch is used for client-side javascript only
-    fetch('http://localhost:3000/weather?address='+address).then((response) => {
+    fetch('/weather?address='+address).then((response) => {
         response.json().then((data) => {
             if(data.error)
                 errorMsg.textContent = data.error
